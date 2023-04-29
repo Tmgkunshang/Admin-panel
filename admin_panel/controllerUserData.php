9864cf0b93cd7d00e4821b98cc1e0a0aadecd4b0
Kunsang Tamang <61765847+Tmgkunshang@users.noreply.github.com>
2023-04-30 00:57:27 +0930

    Add files via upload

---
+++ admin_panel/controllerUserData.php
@@ -0,0 +1,182 @@
+<?php 
+session_start();
+require "./config/dbconnect.php";
+$email = "";
+$username = "";
+$errors = array();
+
+//if admin clicks signup button
+if(isset($_POST['signup'])){
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+    $cpassword = mysqli_real_escape_string($conn, $_POST['cpassword']);
+    if($password !== $cpassword){
+        $errors['password'] = "Confirm password not matched!";
+    }
+    $email_check = "SELECT * FROM adminpanel WHERE email = '$email'";
+    $res = mysqli_query($conn, $email_check);
+    if(mysqli_num_rows($res) > 0){
+        $errors['email'] = "Email that you have entered already exists!";
+    }
+    if(count($errors) === 0){
+        $encpass = password_hash($password, PASSWORD_BCRYPT);
+        $code = rand(999999, 111111);
+        $is_admin = 0;
+        $insert_data = "INSERT INTO adminpanel (username, email, password, code, is_admin)
+                        values('$username', '$email', '$encpass', '$code', '$is_admin')";
+        $data_check = mysqli_query($conn, $insert_data);
+        if($data_check){
+            $subject = "Email Verification Code";
+            $message = "Your verification code is $code";
+            $sender = "From: thapasandeep661@gmail.com";
+            if(mail($email, $subject, $message, $sender)){
+                $info = "We've sent a verification code to your email - $email";
+                $_SESSION['info'] = $info;
+                $_SESSION['email'] = $email;
+                $_SESSION['password'] = $password;
+                header('location: user-otp.php');
+                exit();
+            }else{
+                $errors['otp-error'] = "Failed while sending code!";
+            }
+        }else{
+            $errors['db-error'] = "Failed while inserting data into database!";
+        }
+    }
+
+}
+    //if admin clicks verification code submit button
+    if(isset($_POST['check'])){
+        $_SESSION['info'] = "";
+        $otp_code = mysqli_real_escape_string($conn, $_POST['otp']);
+        $check_code = "SELECT * FROM adminpanel WHERE code = $otp_code";
+        $code_res = mysqli_query($conn, $check_code);
+        if(mysqli_num_rows($code_res) > 0){
+            $fetch_data = mysqli_fetch_assoc($code_res);
+            $fetch_code = $fetch_data['code'];
+            $email = $fetch_data['email'];
+            $code = 0;
+            $is_admin = 1;
+            $update_otp = "UPDATE adminpanel SET code = $code, is_admin = '$is_admin' WHERE code = $fetch_code";
+            $update_res = mysqli_query($conn, $update_otp);
+            if($update_res){
+                $_SESSION['username'] = $username;
+                $_SESSION['email'] = $email;
+                header('location: index.php');
+                exit();
+            }else{
+                $errors['otp-error'] = "Failed while updating code!";
+            }
+        }else{
+            $errors['otp-error'] = "You've entered incorrect code!";
+        }
+    }
+
+    //if admin clicks login button
+    if(isset($_POST['login'])){
+        $email = mysqli_real_escape_string($conn, $_POST['email']);
+        $password = mysqli_real_escape_string($conn, $_POST['password']);
+        $check_email = "SELECT * FROM adminpanel WHERE email = '$email'";
+        $res = mysqli_query($conn, $check_email);
+        if(mysqli_num_rows($res) > 0){
+            $fetch = mysqli_fetch_assoc($res);
+            $fetch_pass = $fetch['password'];
+            if(password_verify($password, $fetch_pass)){
+                $_SESSION['email'] = $email;
+                $is_admin = $fetch['is_admin'];
+                if($is_admin == 1){
+                  $_SESSION['email'] = $email;
+                  $_SESSION['password'] = $password;
+                    header('location: index.php');
+                }else{
+                    $info = "It's look like you haven't still verify your email - $email";
+                    $_SESSION['info'] = $info;
+                    header('location: user-otp.php');
+                }
+            }else{
+                $errors['email'] = "Incorrect email or password!";
+            }
+        }else{
+            $errors['email'] = "It's look like you're not yet an admin! Click on the bottom link to be one.";
+        }
+    }
+
+    //if admin clicks continue button in forgot password form
+    if(isset($_POST['check-email'])){
+        $email = mysqli_real_escape_string($conn, $_POST['email']);
+        $check_email = "SELECT * FROM adminpanel WHERE email='$email'";
+        $run_sql = mysqli_query($conn, $check_email);
+        if(mysqli_num_rows($run_sql) > 0){
+            $code = rand(999999, 111111);
+            $insert_code = "UPDATE adminpanel SET code = $code WHERE email = '$email'";
+            $run_query =  mysqli_query($conn, $insert_code);
+            if($run_query){
+                $subject = "Password Reset Code";
+                $message = "Your password reset code is $code";
+                $sender = "From: thapasandeep661@gmail.com";
+                if(mail($email, $subject, $message, $sender)){
+                    $info = "We've sent a passwrod reset otp to your email - $email";
+                    $_SESSION['info'] = $info;
+                    $_SESSION['email'] = $email;
+                    header('location: reset-code.php');
+                    exit();
+                }else{
+                    $errors['otp-error'] = "Failed while sending code!";
+                }
+            }else{
+                $errors['db-error'] = "Something went wrong!";
+            }
+        }else{
+            $errors['email'] = "This email address does not exist!";
+        }
+    }
+
+    //if admin clicks check reset otp button
+    if(isset($_POST['check-reset-otp'])){
+        $_SESSION['info'] = "";
+        $otp_code = mysqli_real_escape_string($conn, $_POST['otp']);
+        $check_code = "SELECT * FROM adminpanel WHERE code = $otp_code";
+        $code_res = mysqli_query($conn, $check_code);
+        if(mysqli_num_rows($code_res) > 0){
+            $fetch_data = mysqli_fetch_assoc($code_res);
+            $email = $fetch_data['email'];
+            $_SESSION['email'] = $email;
+            $info = "Please create a new password that you don't use on any other site.";
+            $_SESSION['info'] = $info;
+            header('location: new-password.php');
+            exit();
+        }else{
+            $errors['otp-error'] = "You've entered incorrect code!";
+        }
+    }
+
+    //if admin clicks change password button
+    if(isset($_POST['change-password'])){
+        $_SESSION['info'] = "";
+        $password = mysqli_real_escape_string($conn, $_POST['password']);
+        $cpassword = mysqli_real_escape_string($conn, $_POST['cpassword']);
+        if($password !== $cpassword){
+            $errors['password'] = "Confirm password not matched!";
+        }else{
+            $code = 0;
+            $email = $_SESSION['email']; //getting this email using session
+            $encpass = password_hash($password, PASSWORD_BCRYPT);
+            $update_pass = "UPDATE adminpanel SET code = $code, password = '$encpass' WHERE email = '$email'";
+            $run_query = mysqli_query($conn, $update_pass);
+            if($run_query){
+                $info = "Your password changed. Now you can login with your new password.";
+                $_SESSION['info'] = $info;
+                header('Location: password-changed.php');
+            }else{
+                $errors['db-error'] = "Failed to change your password!";
+            }
+        }
+    }
+    
+   //if login now button click
+    if(isset($_POST['login-now'])){
+        header('Location: login.php');
+    }
+
+?>
